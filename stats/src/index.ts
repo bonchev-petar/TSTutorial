a0029e2918a79c08012db8c9ad411f6b7b426413
Petar Bonchev <bonchevpetar@gmail.com>
2021-10-06 16:26:37 +0300

    Finishing with Stats

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,19 +1,8 @@
-import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResult';
+import { MatchReader } from "./MatchReader";
+import { Summary } from "./Summary";
 
-const reader = new CsvFileReader('football.csv');
-reader.read();
+const matchReader = MatchReader.fromCsv("football.csv");
+const summary = Summary.winsAnalysisWithHtmlReport("Crystal Palace");
 
-console.log(reader.data[0][0]);
-
-let manUnitedWins = 0;
-
-for (let match of reader.data) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log('Man United has won: ', manUnitedWins);
+matchReader.load();
+summary.buildAndPrintReport(matchReader.matches);
